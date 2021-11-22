3f658d5a27851f2ed168f4da8dc864f50a66ff2e
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-11-23 01:33:37 +0600

    classes

--- sendbox/src/app.ts
+++ sendbox/src/app.ts
@@ -1,4 +1,31 @@
-const anchor = document.querySelector("a")!;
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// invoices.push({ name: 'shaun' });
+
+console.log(invoices);
+
+const anchor = document.querySelector("a")!; // using ! tell ts `a` anchor exists
 if (anchor) {
   console.log(anchor.href);
 }
