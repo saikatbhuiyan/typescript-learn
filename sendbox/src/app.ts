f984b24d215b1bea3eeacebd5ca12a452843f0f6
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-11-23 01:42:51 +0600

    Public, Private & Readonly

--- sendbox/src/app.ts
+++ sendbox/src/app.ts
@@ -1,14 +1,41 @@
 // classes
-class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+// class Invoice {
+//   client: string;
+//   details: string;
+//   amount: number;
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+//   constructor(c: string, d: string, a: number) {
+//     this.client = c;
+//     this.details = d;
+//     this.amount = a;
+//   }
+
+//   format() {
+//     return `${this.client} owes £${this.amount} for ${this.details}`;
+//   }
+// }
+
+// const invOne = new Invoice("mario", "work on the mario website", 250);
+// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// // invoices.push({ name: 'shaun' });
+
+// console.log(invoices);
+
+// Public, Private & Readonly
+class Invoice {
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
+
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
 
   format() {
     return `${this.client} owes £${this.amount} for ${this.details}`;
@@ -21,9 +48,10 @@ const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-// invoices.push({ name: 'shaun' });
 
-console.log(invoices);
+invoices.forEach((inv) => {
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+});
 
 const anchor = document.querySelector("a")!; // using ! tell ts `a` anchor exists
 if (anchor) {
