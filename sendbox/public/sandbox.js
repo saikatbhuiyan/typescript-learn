49f89d63b3467465a2fa4a69be2d29890bc4cb9f
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-11-23 01:12:31 +0600

    The DOM & Type Casting

--- sendbox/public/sandbox.js
+++ sendbox/public/sandbox.js
@@ -85,3 +85,45 @@ ninja = { name: "yoshi", age: 25 };
 console.log(ninja);
 ninja = { name: 25, age: "yoshi" };
 console.log(ninja);
+// Function Basics
+// let greet: Function = () => {
+//   console.log('hello, world');
+// }
+// greet = 'hello';
+// greet = () => {
+//   console.log('hello, again');
+// }
+const add = (a, b, c = /*?*/ 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, "ninja");
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+console.log(result);
+// Function Signatures
+// example 1
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
+};
+// example 2
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+// example 3
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+};
+const greet = (user) => {
+    console.log("hello, again");
+};
--- sendbox/src/sandbox.ts
+++ sendbox/src/sandbox.ts
@@ -147,6 +147,32 @@ const minus = (a: number, b: number): number => {
 let result = minus(10, 7);
 console.log(result);
 
+// Function Signatures
+// example 1
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+};
+
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === "add") {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+};
+
+// example 3
+let logDetails: (obj: { name: string; age: number }) => void;
+
+logDetails = (ninja: { name: string; age: number }) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+};
+
 // Type Aliases
 type StringOrNum = string | number;
 type objWithName = { name: string; uid: StringOrNum };
