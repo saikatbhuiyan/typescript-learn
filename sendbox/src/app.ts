1ab2b27756901ed0e1e823eceb26edcce87851bb
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-11-23 02:08:13 +0600

    interfaces

--- sendbox/src/app.ts
+++ sendbox/src/app.ts
@@ -1,5 +1,34 @@
 import { Invoice } from "./classes/Invoice.js";
 
+// interfaces
+export interface IsPerson {
+  name: string;
+  age?: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: "shaun",
+  //age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log("I spent ", amount);
+    return amount;
+  },
+};
+
+console.log(me);
+me.speak("hello, world");
+
+const greetPerson = (person: IsPerson): void => {
+  console.log("hello ", person.name);
+};
+
+greetPerson(me);
+
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 
