4d1e8b3ecaa967ffb1f161600487abde8a22ae19
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-12-06 00:12:46 +0600

    typescript compiler

--- typescript-compiler/src/app.ts
+++ typescript-compiler/src/app.ts
@@ -1,1 +1,8 @@
-console.log("Starting");
+interface Keyboard {
+    type: string;
+    modelID: number
+}
+
+const createKeyboard = (modelID: number): Keyboard => {
+    return {type: "Keyboard", modelID}
+}
